a23f5c20918ed186e2341a102f175d25bc489c06
Andy Ron <randy.njfu@gmail.com>
2018-12-19 17:02:07 +0800

    22

---
+++ iOS_Animations_by_Tutorials/22/LockSearch/LockSearch/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeginnerCook
+//
+//  Created by [Andy Ron](https://github.com/andyRon)  on 2018/12/4.
+//  Copyright © 2018 [Andy Ron](https://github.com/andyRon) . All rights reserved.
+//
+
+
+import UIKit
+
+func delay(seconds: Double, completion: @escaping ()-> Void) {
+  DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: completion)
+}
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+}
---
+++ iOS_Animations_by_Tutorials/22/LockSearch/LockSearch/WidgetsOwnerProtocol.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BeginnerCook
+//
+//  Created by [Andy Ron](https://github.com/andyRon)  on 2018/12/4.
+//  Copyright © 2018 [Andy Ron](https://github.com/andyRon) . All rights reserved.
+//
+
+
+import UIKit
+
+protocol WidgetsOwnerProtocol {
+  var blurView: UIVisualEffectView {get}
+
+  func startPreview(for: UIView)
+  func updatePreview(percent: CGFloat)
+  func finishPreview()
+  func cancelPreview()
+}
+
+extension WidgetsOwnerProtocol {
+  func startPreview(for forView: UIView) { }
+  func updatePreview(percent: CGFloat) { }
+  func finishPreview() { }
+  func cancelPreview() { }
+}
